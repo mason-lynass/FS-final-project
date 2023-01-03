2252bf84cd7ec547a359f701a8086e12e1bbcb22
Mason <masonlynass@gmail.com>
2023-01-02 16:15:39 -0800

    more fly stuff

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   get "/me", to: "users#show"
   post "/signup", to: "users#create"
 
+  # get '*path', to: "application#fallback_index_html", constraints: -> (request) do 
+  #   !request.xhr? && request.format.html?
+  # end
+
   # get "*path", to: "application#hello_world"
 end
